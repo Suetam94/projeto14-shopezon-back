7e4325be6c1e9c781e3f02301f8fe8820bfd4e92
Mateus Vinicius da Silva <mateusviniciusdasilva@outlook.com>
2022-07-05 21:44:20 -0300

    initial commit

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const port = process.env.PORT || 5000;
+
+app.listen(port, () =>
+  console.log(`Server running at http://localhost:${port}`)
+);
